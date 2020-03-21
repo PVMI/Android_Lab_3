ad1d73bbbbe8767eae445da7ea18dc1f44ca06bb
PVMI <pvmi0604@gmail.com>
2020-03-21 17:45:38 -0600

    Commit QuizActivity and ActivityTest Kotlin

---
+++ app/src/main/java/com/abc/android_lab_3/ActivityTest.kt
@@ -0,0 +1,11 @@
+package com.abc.android_lab_3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActivityTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
